547136a1fc2d1427a822b9baa9a2121eedc15111
Eric Seynaeve <eric@seynaeve.be>
2022-03-10 13:31:21 +0100

    Samenvatting geschreven.

---
+++ text/ondersteunende_documenten/Kosten van landbouw - Sectoren voedselketen.mm
@@ -0,0 +1,138 @@
+<map version="freeplane 1.9.13">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Sectoren voedselketen" FOLDED="false" ID="ID_1803088154" CREATED="1646817447721" MODIFIED="1646845186897">
+<edge COLOR="#7c0000"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" associatedTemplateLocation="template:/standard-1.6.mm" show_note_icons="true" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm" VGAP_QUANTITY="2 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="1" DASH="SOLID"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode TEXT="Landwijzer werkstuk" STYLE="oval">
+<font NAME="LM Roman 10" SIZE="18"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="LM Roman 10" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font NAME="LM Roman 10" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font NAME="LM Roman 10" SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font NAME="LM Roman 10" SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font NAME="LM Roman 10" SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<font NAME="LM Roman 10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<font NAME="LM Roman 10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<font NAME="LM Roman 10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<font NAME="LM Roman 10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="23" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="Landbouw" POSITION="left" ID="ID_1611978945" CREATED="1646815327944" MODIFIED="1646817459429" VGAP_QUANTITY="2 pt">
+<node TEXT="Bestrijdingsmiddelen" ID="ID_1640055620" CREATED="1646816239193" MODIFIED="1646816245732"/>
+<node TEXT="Dienstverlening" ID="ID_528503883" CREATED="1646816271017" MODIFIED="1646816276637"/>
+<node TEXT="Dierlijke mest" ID="ID_667142357" CREATED="1646815505335" MODIFIED="1646816207813"/>
+<node TEXT="Drogen" ID="ID_1401396180" CREATED="1646817094336" MODIFIED="1646817096406"/>
+<node TEXT="Elektronica" ID="ID_1157442781" CREATED="1646816265569" MODIFIED="1646816270572"/>
+<node TEXT="Gebouwen" ID="ID_660844855" CREATED="1646815445929" MODIFIED="1646815482234"/>
+<node TEXT="Geneesmiddelen" ID="ID_169511797" CREATED="1646816246224" MODIFIED="1646816253715"/>
+<node TEXT="Infrastructuur" ID="ID_408550857" CREATED="1646816286416" MODIFIED="1646816291868"/>
+<node TEXT="Irrigatie" ID="ID_1853281168" CREATED="1646817096705" MODIFIED="1646817098630"/>
+<node TEXT="Kunstmest" ID="ID_47923455" CREATED="1646815513583" MODIFIED="1646816220430"/>
+<node TEXT="Micronutrienten en spoorelementen" ID="ID_1922814201" CREATED="1646816221016" MODIFIED="1646816238678"/>
+<node TEXT="Mijnbouw" ID="ID_680625881" CREATED="1646816277128" MODIFIED="1646816280453"/>
+<node TEXT="Transport" ID="ID_694150722" CREATED="1646816280969" MODIFIED="1646816286020"/>
+<node TEXT="Trekkers en machines" ID="ID_1589344849" CREATED="1646815482711" MODIFIED="1646815500482"/>
+<node TEXT="Veevoer" ID="ID_1824780428" CREATED="1646815500887" MODIFIED="1646815504914"/>
+<node TEXT="Zaaizaad, poot- en plantgoed" ID="ID_1233870800" CREATED="1646816254144" MODIFIED="1646816265046"/>
+</node>
+<node TEXT="Verwerking" POSITION="left" ID="ID_1852021499" CREATED="1646816297937" MODIFIED="1646817462007">
+<node TEXT="Afval" ID="ID_1206153795" CREATED="1646820239489" MODIFIED="1646820240967"/>
+<node TEXT="Dienstverlening" ID="ID_704622904" CREATED="1646816271017" MODIFIED="1646816276637"/>
+<node TEXT="Elektronica" ID="ID_729030549" CREATED="1646816265569" MODIFIED="1646816270572"/>
+<node TEXT="Gebouwen" ID="ID_1452152270" CREATED="1646815445929" MODIFIED="1646815482234"/>
+<node TEXT="Infrastructuur" ID="ID_1441058089" CREATED="1646816286416" MODIFIED="1646816291868"/>
+<node TEXT="Mijnbouw" ID="ID_1160452064" CREATED="1646816277128" MODIFIED="1646816280453"/>
+<node TEXT="Transport" ID="ID_606505423" CREATED="1646816280969" MODIFIED="1646816286020"/>
+<node TEXT="Verpakking" ID="ID_705664282" CREATED="1646817102225" MODIFIED="1646817669174"/>
+</node>
+<node TEXT="Consument" POSITION="right" ID="ID_913216357" CREATED="1646816316064" MODIFIED="1646821223932">
+<edge COLOR="#ff0000"/>
+<node TEXT="Afval" ID="ID_818588348" CREATED="1646820131888" MODIFIED="1646820134142"/>
+<node TEXT="Bereiding" ID="ID_1105088736" CREATED="1646817117912" MODIFIED="1646819149263"/>
+<node TEXT="Bewaring" ID="ID_510840144" CREATED="1646817114832" MODIFIED="1646817133372"/>
+<node TEXT="Dienstverlening" ID="ID_943190777" CREATED="1646816271017" MODIFIED="1646816276637"/>
+<node TEXT="Elektronica" ID="ID_1008023755" CREATED="1646816265569" MODIFIED="1646816270572"/>
+<node TEXT="Gebouwen" ID="ID_1469931111" CREATED="1646815445929" MODIFIED="1646815482234"/>
+<node TEXT="Infrastructuur" ID="ID_126297498" CREATED="1646816286416" MODIFIED="1646816291868"/>
+<node TEXT="Mijnbouw" ID="ID_1693687753" CREATED="1646816277128" MODIFIED="1646816280453"/>
+<node TEXT="Transport" ID="ID_99635008" CREATED="1646816280969" MODIFIED="1646816286020"/>
+</node>
+<node TEXT="Distributie" POSITION="right" ID="ID_928778959" CREATED="1646816311560" MODIFIED="1646821221120">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Afval" ID="ID_123538628" CREATED="1646820232881" MODIFIED="1646820234071"/>
+<node TEXT="Bewaring" ID="ID_1220580053" CREATED="1646817108008" MODIFIED="1646817635637"/>
+<node TEXT="Dienstverlening" ID="ID_1343664116" CREATED="1646816271017" MODIFIED="1646816276637"/>
+<node TEXT="Elektronica" ID="ID_1380246984" CREATED="1646816265569" MODIFIED="1646816270572"/>
+<node TEXT="Gebouwen" ID="ID_1819546680" CREATED="1646815445929" MODIFIED="1646815482234"/>
+<node TEXT="Infrastructuur" ID="ID_678540840" CREATED="1646816286416" MODIFIED="1646816291868"/>
+<node TEXT="Mijnbouw" ID="ID_467149254" CREATED="1646816277128" MODIFIED="1646816280453"/>
+<node TEXT="Transport" ID="ID_680066076" CREATED="1646816280969" MODIFIED="1646816286020"/>
+<node TEXT="Verpakking" ID="ID_1243144500" CREATED="1646817110216" MODIFIED="1646817666350"/>
+</node>
+</node>
+</map>
