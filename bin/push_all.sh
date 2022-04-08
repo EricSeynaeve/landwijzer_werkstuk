03b737bae8626b54296d72fe97758979ee6bb859
Eric Seynaeve <github@nosperse.com>
2022-04-08 12:57:00 +0100

    Beginnen van kortere tekst

--- bin/push_all.sh
+++ bin/push_all.sh
@@ -15,9 +15,25 @@ sed -i 's#^\\papersides .*#\\papersides 1#' manuscript_single.lyx
 sed -i 's#^\\usepackage{lscape}.*#\\usepackage{pdflscape}#' manuscript_single.lyx
 awk -v RS="\0" -v ORS=""  '{gsub(/manusc\nript\.pdf/,"manusc\nript_single.pdf"); print}' manuscript_single.lyx > manuscript_single.$$ && mv manuscript_single.$$ manuscript_single.lyx
 awk -v RS="\0" -v ORS=""  '{gsub(/manuscrip\nt\.pdf/,"manuscrip\nt_single.pdf"); print}' manuscript_single.lyx > manuscript_single.$$ && mv manuscript_single.$$ manuscript_single.lyx
-diff -u manuscript.lyx manuscript_single.lyx
+diff -u manuscript.lyx manuscript_single.lyx || true
 time /usr/bin/lyx --export pdf2 manuscript_single.lyx && git add manuscript_single.pdf
 rm manuscript_single.lyx
+
+cowsay "Creating FULL pdf's"
+cd "$HOME/Nextcloud/Landwijzer/True cost of food/text"
+
+cowsay -f small "Double sided (default)"
+time /usr/bin/lyx --export pdf2 manuscript-full.lyx && git add manuscript-full.pdf
+
+cowsay -f small "Single sided"
+cp -f manuscript-full.lyx manuscript-full_single.lyx
+sed -i 's#^\\papersides .*#\\papersides 1#' manuscript-full_single.lyx
+sed -i 's#^\\usepackage{lscape}.*#\\usepackage{pdflscape}#' manuscript-full_single.lyx
+awk -v RS="\0" -v ORS=""  '{gsub(/manusc\nript\.pdf/,"manusc\nript_single.pdf"); print}' manuscript-full_single.lyx > manuscript-full_single.$$ && mv manuscript-full_single.$$ manuscript-full_single.lyx
+awk -v RS="\0" -v ORS=""  '{gsub(/manuscrip\nt\.pdf/,"manuscrip\nt_single.pdf"); print}' manuscript-full_single.lyx > manuscript-full_single.$$ && mv manuscript-full_single.$$ manuscript-full_single.lyx
+diff -u manuscript-full.lyx manuscript-full_single.lyx || true
+time /usr/bin/lyx --export pdf2 manuscript-full_single.lyx && git add manuscript-full_single.pdf
+rm manuscript-full_single.lyx
 
 cowsay "Commit & push"
 git commit
